ccf934d46d32a979d7120f528f78e6a743d7747f
Hiroki Sato <resumi30minutes@gmail.com>
2023-09-21 21:24:05 +0900

    fix edit forms

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -15,14 +15,15 @@ class User::RegistrationsController < Devise::RegistrationsController
   end
 
   # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  def edit
+    super
+  end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+    p "こんにちは"
+    super
+  end
 
   # DELETE /resource
   # def destroy
