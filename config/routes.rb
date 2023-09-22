96d3327f83de7fc00b96564f87239a1a56836a00
Hiroki Sato <resumi30minutes@gmail.com>
2023-09-22 19:18:10 +0900

    create boards controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
     sessions: 'user/sessions',
     registrations: 'user/registrations'
   }
+
+  resource :board
+
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   root "static_pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
 end
