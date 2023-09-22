aee1a74fc73ddd1b32afe63ea645eceff406e63d
Hiroki Sato <resumi30minutes@gmail.com>
2023-09-22 21:05:56 +0900

    flash messages

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,5 +1,5 @@
 class BoardsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destory]
   def new
     @board = Board.new
   end
@@ -7,8 +7,10 @@ class BoardsController < ApplicationController
   def create
     @board = Board.new(title: board_params[:title], user_id: current_user.id)
     if @board.save
+      flash[:notice] = "board created"
       redirect_to board_path(@board.id)
     else
+      flash.now[:alert] = "empty board name"
       render 'new', status: :unprocessable_entity
     end
   end
