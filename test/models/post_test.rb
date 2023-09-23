959b873b11ea7406ee418c6b2d081f0496ac3c70
Hiroki Sato <resumi30minutes@gmail.com>
2023-09-23 23:43:25 +0900

    add post test

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  def setup
+    @user = users(:one)
+    @board = boards(:one)
+    @post = @board.posts.build(content: "Test")
+  end
+
+  test 'should be valid' do
+    @post.user_id = @user.id
+    @post.valid?
   end
 end
