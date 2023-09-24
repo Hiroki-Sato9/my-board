4ff7387faa0e5628a00d5dd7c2a7fadfcf8b608c
Hiroki Sato <resumi30minutes@gmail.com>
2023-09-24 19:03:36 +0900

    add post order test

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :board
+  default_scope -> { order(created_at: :desc) }
 
   validates :user_id, presence: true
   validates :board_id, presence: true
+  validates :content, presence: true
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -5,10 +5,29 @@ class PostTest < ActiveSupport::TestCase
     @user = users(:one)
     @board = boards(:one)
     @post = @board.posts.build(content: "Test")
+    @post.user_id = @user.id
   end
 
   test 'should be valid' do
-    @post.user_id = @user.id
-    @post.valid?
+    assert @post.valid?
+  end
+
+  test 'user id should be present' do
+    @post.user_id = nil
+    refute @post.valid?
+  end
+
+  test 'board id should be present' do
+    @post.board_id = nil
+    refute @post.valid?
+  end
+
+  test 'content should be present' do
+    @post.content = nil
+    refute @post.valid?
+  end
+
+  test 'order should be most recent first' do
+    assert_equal posts(:most_rescent), Post.first
   end
 end
